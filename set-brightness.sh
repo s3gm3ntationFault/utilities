aa386cbb6e95633a6d61235703f6b7071d6d637e
s3gm3ntationFault <36698770+s3gm3ntationFault@users.noreply.github.com>
2019-04-22 10:53:09 +0200

    set-brightness script
    This script let change the screen brightness

---
+++ set-brightness.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+BASEPATH=/sys/class/backlight
+DEVICE="intel_backlight"
+cd $BASEPATH
+MAX=`cat ${DEVICE}/max_brightness`
+CURRENT=`cat ${DEVICE}/actual_brightness`
+PERCENT=`echo $MAX/100 | bc`
+
+echo $PERCENT
+if [ "$#" -ne 2 ]; then
+    echo "Error: wrong parameters"
+    echo "Run: # set-brightness.sh inc/dec percentage"
+    exit -1
+fi
+
+MODE=$1
+DELTA=$2
+if [ "$MODE" = "inc" ]; then
+    # Increase brightness an x%
+    NEW_BRIGHT="$(( $CURRENT + ( $PERCENT * $DELTA ) ))"
+    if [ $NEW_BRIGHT -gt $MAX ]; then
+        NEW_BRIGHT=$MAX
+    fi
+elif [ "$MODE" = "dec" ]; then
+    # Decrease brightness an x%
+    NEW_BRIGHT="$(( $CURRENT - ( $PERCENT * $DELTA) ))"
+    if [ $NEW_BRIGHT -lt 0 ]; then
+        NEW_BRIGHT=0
+    fi
+else
+    echo "Option: $MODE does not exists. Please use inc or dec"
+    exit -1
+fi
+# Set the new bright
+echo $NEW_BRIGHT
+echo $NEW_BRIGHT > ${DEVICE}/brightness
+exit 0
